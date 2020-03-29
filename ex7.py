5e8e1a62a1fbafd354bb22714cc1faa007ef3fe9
androsfat <androsfat@hotmail.com>
2020-03-29 23:31:19 +0300

    More exercises with variables and strings

---
+++ ex7.py
@@ -0,0 +1,25 @@
+print("Mary had a lamb.")
+# print a farmatted string
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+# print 10 times a dot
+print("." * 10)  # what'd that do?
+
+end1 = "C"
+en2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch end = ' ' at the end.  try removing it to see what happens
+# I am not sure but it looks like the end is a keyword
+# and joins the 2 print statements together
+print(end1 + en2 + end3 + end4 + end5 + end6, end=' ')
+print(end7 + end8 + end9 + end10 + end11 + end12)
--- readme.md
+++ readme.md
@@ -1,6 +1,4 @@
 ## Learn Python The Hard Way
 
 Following the book tutorial "Learn Python The Hard Way"  
-Well... I hope I will keep until the end!
-
-Andros Kalou
+Well... I hope I will keep it until the end!
