ad77e8383c4e813108cfc46e9ec40944691ff50f
androsfat <androsfat@hotmail.com>
2020-04-02 17:24:30 +0300

    Refactor the script to just one line of code. All previous code is commented out

--- 17_more_files.py
+++ 17_more_files.py
@@ -3,22 +3,22 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print(f"Copying from {from_file} to {to_file}")
+# print(f"Copying from {from_file} to {to_file}")
 
 # we could do these two on one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+# in_file = open(from_file)
+# indata = open(from_file).read()
 
-print(f"The input file is {len(indata)} bytes long")
+# print(f"The input file is {len(indata)} bytes long")
 
-print(f"Does the output file exist? {exists(to_file)}")
-print("Ready, hit RETURN to continue, CTRL-C to abort.")
+# print(f"Does the output file exist? {exists(to_file)}")
+print(f"Copy {from_file} to {to_file},\nHit RETURN to continue, CTRL-C to abort.")
 input()
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+# out_file = open(to_file, 'w')
+open(to_file, 'w').write(open(from_file).read())
 
-print("Alright, all done.")
+print("Done!")
 
-out_file.close()
-in_file.close()
+# out_file.close()
+# in_file.close()
