a550124a1e54f7f866850a458abc84a5edd5947e
Andros Kalou <androsfat@hotmail.com>
2020-04-24 23:10:22 +0300

    add exercise 35 with fixes

--- 35_branches_and_functions.py
+++ 35_branches_and_functions.py
@@ -41,7 +41,7 @@ def bear_room():
 
 
 def cthulhu_room():
-    print("Here you see the great evil Cthulthu.")
+    print("Here you see the great evil Cthulhu.")
     print("He, it, whatever stares at you and you go insane.")
     print("Do you flee for your life or eat your head?")
 
